f6246bac416b43cb98041872f5bd4d860bf5adf3
Lauri Koskela <luryus@users.noreply.github.com>
2021-08-19 21:56:15 +0300

    Add SecretTextView ui component

---
+++ src/ui/components/secret_text_view.rs
@@ -0,0 +1,55 @@
+use cursive::{theme::Style, utils::span::SpannedString, View};
+use zeroize::Zeroizing;
+
+pub struct SecretTextView {
+    content: Zeroizing<String>,
+    hidden: bool,
+    style: Style,
+}
+
+impl SecretTextView {
+    pub fn new(content: String) -> Self {
+        SecretTextView {
+            content: Zeroizing::new(content),
+            hidden: true,
+            style: Style::none(),
+        }
+    }
+
+    pub fn hidden(mut self) -> Self {
+        self.hidden = true;
+        self
+    }
+
+    pub fn visible(mut self) -> Self {
+        self.hidden = false;
+        self
+    }
+
+    pub fn style<S: Into<Style>>(mut self, style: S) -> Self {
+        self.style = style.into();
+        self
+    }
+
+    pub fn set_hidden(&mut self, hidden: bool) {
+        self.hidden = hidden;
+    }
+
+    pub fn toggle_hidden(&mut self) {
+        self.hidden = !self.hidden;
+    }
+}
+
+impl View for SecretTextView {
+    fn draw(&self, printer: &cursive::Printer) {
+        let styled = SpannedString::styled(
+            if self.hidden {
+                "*******"
+            } else {
+                &self.content
+            },
+            self.style,
+        );
+        printer.print_styled((0, 0), (&styled).into());
+    }
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,3 +3,4 @@ pub mod data;
 pub mod item_details;
 pub mod login;
 pub mod vault_table;
+pub mod components;
