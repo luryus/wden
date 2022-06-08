14e7a70bbf546fa9b002c4357993b505c600491f
Lauri Koskela <luryus@users.noreply.github.com>
2022-06-08 17:33:43 +0300

    Refactor x11 clipboard stuff into its own submodule

--- wden/src/ui/clipboard/linux_clipboard.rs
+++ wden/src/ui/clipboard/linux_clipboard.rs
@@ -1,20 +1,38 @@
-use std::{collections::HashMap, sync::MutexGuard, time::Duration};
-
 use super::{PlatformClipboard, PlatformClipboardResult};
-use anyhow::Context;
-use lazy_static::lazy_static;
-use x11rb::{connection::Connection, protocol::xproto::ConnectionExt};
-use std::sync::Mutex;
-use x11_clipboard::{Clipboard, Atom};
-
-lazy_static! {
-    static ref CLIPBOARD: Mutex<Option<Clipboard>> = Mutex::new(None);
-}
-
 pub struct LinuxClipboard;
 
 impl PlatformClipboard for LinuxClipboard {
     fn clip_string(s: String) -> PlatformClipboardResult<()> {
+        x11::clip_string(s)
+    }
+
+    fn get_string_contents() -> PlatformClipboardResult<String> {
+        x11::get_string_contents()
+    }
+
+    fn clear() -> PlatformClipboardResult<()> {
+        Self::clip_string(String::new())
+    }
+}
+
+mod x11 {
+    use super::PlatformClipboardResult;
+    use anyhow::Context;
+    use lazy_static::lazy_static;
+    use std::collections::HashMap;
+    use std::{
+        sync::{Mutex, MutexGuard},
+        time::Duration,
+    };
+
+    use x11_clipboard::{Atom, Clipboard};
+    use x11rb::{connection::Connection, protocol::xproto::ConnectionExt};
+
+    lazy_static! {
+        static ref CLIPBOARD: Mutex<Option<Clipboard>> = Mutex::new(None);
+    }
+
+    pub fn clip_string(s: String) -> PlatformClipboardResult<()> {
         let cb = get_cb()?;
 
         let cb = cb.as_ref().unwrap();
@@ -30,7 +48,7 @@ impl PlatformClipboard for LinuxClipboard {
         Ok(())
     }
 
-    fn get_string_contents() -> PlatformClipboardResult<String> {
+    pub fn get_string_contents() -> PlatformClipboardResult<String> {
         let cb = get_cb()?;
 
         let cb = cb.as_ref().unwrap();
@@ -44,21 +62,17 @@ impl PlatformClipboard for LinuxClipboard {
         String::from_utf8(val).context("Parsing UTF-8 string failed")
     }
 
-    fn clear() -> PlatformClipboardResult<()> {
-        Self::clip_string(String::new())
-    }
-}
+    fn get_cb() -> Result<MutexGuard<'static, Option<Clipboard>>, anyhow::Error> {
+        let mut cb_opt = CLIPBOARD.lock().unwrap();
+        if cb_opt.is_none() {
+            *cb_opt = Some(Clipboard::new()?);
+        }
 
-fn get_cb() -> Result<MutexGuard<'static, Option<Clipboard>>, anyhow::Error> {
-    let mut cb_opt = CLIPBOARD.lock().unwrap();
-    if cb_opt.is_none() {
-        *cb_opt = Some(Clipboard::new()?);
+        Ok(cb_opt)
     }
 
-    Ok(cb_opt)
-}
-
-fn get_kde_password_hint_atom(connection: &impl Connection) -> PlatformClipboardResult<Atom> {
-    let cookie = connection.intern_atom(false, b"x-kde-passwordManagerHint")?;
-    Ok(cookie.reply()?.atom)
+    fn get_kde_password_hint_atom(connection: &impl Connection) -> PlatformClipboardResult<Atom> {
+        let cookie = connection.intern_atom(false, b"x-kde-passwordManagerHint")?;
+        Ok(cookie.reply()?.atom)
+    }
 }
